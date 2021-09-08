52aaf0c8114bcc0ab9de394357a1af16918a6eff
jorgeepena <jorgepena@aragorn-elessar.local>
2021-09-08 18:21:30 -0500

    First commit

---
+++ resources/views/archivo.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nombre</title>
+</head>
+<body>
+	<h1>Probando Sumas</h1>
+
+	@php
+		$suma = 5+5;
+	@endphp
+
+	<h5>Resultado: {{ $suma }}</h5>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Mi proyecto</title>
+</head>
+<body>
+    <h1>Hola Mundo</h1>
+</body>
+</html>
